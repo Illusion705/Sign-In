06df3b4626d7e3cd490a985e72446c5b8ccddfa4
Illusion705 <65733710+Illusion705@users.noreply.github.com>
2020-08-17 19:18:20 -0400

    Update code.py
    Changed the username valid characters list to a string.

--- code.py
+++ code.py
@@ -64,7 +64,7 @@ def create_account():
 			if not username in users:
 				index1 = 0
 				while index1 < len(username) - 1:
-					if username[index1] in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "_"]:
+					if username[index1] in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_":
 						valid_username = True
 					else:
 						valid_username = False
